5409c8578b68b83650848f7156a3735e140f420c
Luan <luancgs.dev@pm.me>
2025-11-11 00:19:14 -0300

    feat: create input utils module to get input

---
+++ utils/input/input.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const fs = std.fs;
+
+const year = 2016;
+
+pub fn get_input(buffer: *const []u8, day: comptime_int) anyerror!usize {
+    const file_buffer = try std.heap.page_allocator.alloc(u8, 1024);
+    defer std.heap.page_allocator.free(file_buffer);
+
+    const cwd = fs.cwd();
+    const open_file_flags = fs.File.OpenFlags{ .mode = .read_only };
+
+    var filename_buffer: [64]u8 = undefined;
+    const file_path = try std.fmt.bufPrint(&filename_buffer, "./inputs/{d}/day0{d}.txt", .{ year, day });
+    const file = try cwd.openFile(file_path, open_file_flags);
+    const bytes_read = try file.read(file_buffer);
+
+    _ = try std.fmt.bufPrint(buffer.*, "{s}\n", .{file_buffer[0..bytes_read]});
+
+    return bytes_read;
+}
